af37226c4486820b76f3050735e0c8d54eb98a6f
sagar0840 <sagarkale55556@gmail.com>
2023-02-11 12:53:55 +0530

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, rem, sum = 0, temp;
+             
+            Console.WriteLine(" To Find a Number is Palindrome or not  ");
+            Console.Write("Enter a number ");
+            num = Convert.ToInt32(Console.ReadLine());
+            temp = num;
+            while (num > 0)
+            {
+                rem = num % 10;     
+                num = num / 10;  
+                sum = sum * 10 + rem;
+                
+            }
+            Console.WriteLine(" The Reversed Number is: {0} ", sum);
+            if (temp == sum)    
+            {
+                Console.WriteLine(" Number is Palindrome ");
+            }
+            else
+            {
+                Console.WriteLine(" Number is not a palindrome ");
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
